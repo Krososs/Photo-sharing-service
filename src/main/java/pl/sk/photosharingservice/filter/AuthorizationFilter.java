bad90b2c7a4cc03a0c2a535979897eb81838745a
Krososs <krososmail@gmail.com>
2022-07-04 15:25:24 +0200

    AuthorizationFilter-refactor

--- src/main/java/pl/sk/photosharingservice/filter/AuthorizationFilter.java
+++ src/main/java/pl/sk/photosharingservice/filter/AuthorizationFilter.java
@@ -3,13 +3,12 @@ package pl.sk.photosharingservice.filter;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import pl.sk.photosharingservice.support.AuthUtil;
-
+import pl.sk.photosharingservice.support.ValidationUtil;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,11 +17,10 @@ import java.io.IOException;
 import java.util.*;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@Slf4j
 public class AuthorizationFilter extends OncePerRequestFilter {
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -31,33 +29,26 @@ public class AuthorizationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request,response);
         }else{
             String authHeader = request.getHeader(AUTHORIZATION);
-            if(authHeader!=null && authHeader.startsWith("e ")){
+            if(authHeader!=null){
                 try{
-                    String token = authHeader.substring("e ".length());
-                    DecodedJWT jwt = JWT.decode(token);
+                    DecodedJWT jwt = JWT.decode(authHeader);
 
                     if( jwt.getExpiresAt().before(new Date())) {
-                        Map<String, String> error = new HashMap<>();
-                        error.put("error_message", "Token is expired");
                         response.setContentType(APPLICATION_JSON_VALUE);
-                        response.setStatus(401);
-                        new ObjectMapper().writeValue(response.getOutputStream(), error);
+                        response.setStatus(UNAUTHORIZED.value());
+                        new ObjectMapper().writeValue(response.getOutputStream(), ValidationUtil.getErrorResponse(UNAUTHORIZED.value(), "Token is expired"));
                     }
 
                     Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                    authorities.add(new SimpleGrantedAuthority(AuthUtil.getRolesFromToken(token)));
+                    authorities.add(new SimpleGrantedAuthority(AuthUtil.getRolesFromToken(authHeader)));
                     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(jwt.getSubject(),null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                     filterChain.doFilter(request, response);
 
                 }catch (Exception exception){
-
-                    response.setHeader("error", exception.getMessage());
-                    response.setStatus(FORBIDDEN.value());
-                    Map<String, String> error = new HashMap<>();
-                    error.put("error_message",exception.getMessage());
                     response.setContentType(APPLICATION_JSON_VALUE);
-                    new ObjectMapper().writeValue(response.getOutputStream(), error);
+                    response.setStatus(FORBIDDEN.value());
+                    new ObjectMapper().writeValue(response.getOutputStream(), ValidationUtil.getErrorResponse(UNAUTHORIZED.value(), exception.getMessage()));
                 }
 
             }else{
@@ -65,7 +56,5 @@ public class AuthorizationFilter extends OncePerRequestFilter {
 
             }
         }
-
-
     }
 }
--- src/main/java/pl/sk/photosharingservice/support/AuthUtil.java
+++ src/main/java/pl/sk/photosharingservice/support/AuthUtil.java
@@ -15,12 +15,11 @@ public class  AuthUtil {
     private final static Algorithm algorithm = Algorithm.HMAC256("secrect".getBytes());
     private final static JWTVerifier verifier = JWT.require(algorithm).build();
 
-
     public static String getAccesToken(UserDetails appUser){
 
         return JWT.create()
                 .withSubject(appUser.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis()+1000*60*1000)) //16 hh ?
+                .withExpiresAt(new Date(System.currentTimeMillis()+1000*60*60*16)) //16h
                 .withClaim("roles", appUser.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining()))
                 .sign(algorithm);
     }
@@ -28,12 +27,11 @@ public class  AuthUtil {
     public static String getRefreshToken(UserDetails appUser) {
         return JWT.create()
                 .withSubject(appUser.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10000 * 60 * 1000)) //166h?
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10000 * 60 * 60 * 100)) //100h
                 .sign(algorithm);
     }
 
-    public static String getUsernameFromToken(String _token){
-        String token = _token.substring("e ".length());
+    public static String getUsernameFromToken(String token){
         DecodedJWT decodedJWT = verifier.verify(token);
         return decodedJWT.getSubject();
     }
